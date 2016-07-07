afe8421fbdf6e7ad620f697f217bafefdba26dd0
arocketman <andrealfie@gmail.com>
2016-07-07 13:31:05 +0200

    Switched from many labels to listViews  with ObservableList

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,22 +1,17 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import sample.core.CPU;
 import sample.core.Sim;
 
 public class Controller {
 
-    @FXML
-    private VBox Aregs;
-    @FXML
-    private VBox Dregs;
     @FXML
     private TextArea Instructions;
     @FXML
@@ -29,6 +24,20 @@ public class Controller {
     private Button getCommand;
     @FXML
     private Label SR;
+    @FXML
+    private ListView<String> AListView;
+    @FXML
+    private ListView<String> DListView;
+    @FXML
+    private ListView<String> OtherRegsListView;
+
+    private static int STATUS_REGISTER = 0;
+
+    ObservableList<String> ARegsObservable = FXCollections.observableArrayList();
+    ObservableList<String> DRegsObservable = FXCollections.observableArrayList();
+    ObservableList<String> otherRegsObservable = FXCollections.observableArrayList();
+
+
 
     Sim system = new Sim();
 
@@ -37,12 +46,17 @@ public class Controller {
 
         //Registers setup
         for(int i = 0; i <= 7; i++){
-            ((Label)Aregs.getChildren().get(i)).setText("A" + i + "=" + getHexWithTrailingZeroes(system.getCpu().getA(i)));
-            ((Label)Dregs.getChildren().get(i)).setText("D" + i + "=" + getHexWithTrailingZeroes(system.getCpu().getD(i)));
+            String AregI="A" + i + "=" + getHexWithTrailingZeroes(system.getCpu().getA(i));
+            String DregI = "D" + i + "=" + getHexWithTrailingZeroes(system.getCpu().getD(i));
+            ARegsObservable.add(AregI);
+            DRegsObservable.add(DregI);
         }
 
-        SR.setText(getBinWithTrailingZeroes((int) system.getCpu().getSR()));
-        SR.setMinWidth(120);
+        AListView.setItems(ARegsObservable);
+        DListView.setItems(DRegsObservable);
+        OtherRegsListView.setItems(otherRegsObservable);
+
+        otherRegsObservable.add(STATUS_REGISTER,"SR = " + getBinWithTrailingZeroes((int) system.getCpu().getSR()));
 
         //Textarea setup
         Instructions.setEditable(false);
@@ -62,13 +76,9 @@ public class Controller {
                 else if(instruction.getSelectionModel().getSelectedItem().toString() == "ADD")
                     system.getCpu().getInstructionSet().regToReg("ADD",source.getSelectionModel().getSelectedItem().toString(),destination.getSelectionModel().getSelectedItem().toString());
 
-                int sindex = source.getSelectionModel().getSelectedIndex();
                 int dindex = destination.getSelectionModel().getSelectedIndex();
-                Label lsindex = (Label)Dregs.getChildren().get(sindex);
-                Label ldindex = (Label)Dregs.getChildren().get(dindex);
-                lsindex.setText(lsindex.getText().substring(0,3) + getHexWithTrailingZeroes(system.getCpu().getD(sindex)));
-                ldindex.setText(ldindex.getText().substring(0,3) + getHexWithTrailingZeroes(system.getCpu().getD(dindex)));
-                SR.setText(getBinWithTrailingZeroes((int) system.getCpu().getSR()));
+                DRegsObservable.set(dindex,DRegsObservable.get(dindex).substring(0,3) + getHexWithTrailingZeroes(system.getCpu().getD(dindex)));
+                otherRegsObservable.set(STATUS_REGISTER,getBinWithTrailingZeroes((int) system.getCpu().getSR()));
 
                 Instructions.appendText(instruction.getSelectionModel().getSelectedItem().toString() + " " + source.getSelectionModel().getSelectedItem().toString() + "," + destination.getSelectionModel().getSelectedItem().toString() + "\n");
             }
